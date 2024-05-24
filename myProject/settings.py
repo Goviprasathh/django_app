86ec253c619875c98dbf2ff93a4a7a633e25c411
Goviprasathh <142871709+Goviprasathh@users.noreply.github.com>
2024-05-24 17:40:09 +0530

    Add files via upload

--- myProject/settings.py
+++ myProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-g*yiw!i5uq*7(-dhtdbkm=52!=k-*dg5iiq_vnc*3h%c#pxyrq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'myProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
